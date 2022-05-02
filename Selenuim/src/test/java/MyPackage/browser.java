a34a187c79cc43e0f7926767f1694bd63809a6cb
ghazikh <ing.ghazi.khazri@gmail.com>
2022-05-02 23:05:20 +0100

    new commit 02/05/2022

--- Selenuim/src/test/java/MyPackage/browser.java
+++ Selenuim/src/test/java/MyPackage/browser.java
@@ -15,7 +15,7 @@ public class browser {
         	//chrome browser
 //           System.setProperty("webdriver.chrome.driver","C:\\Users\\ghazi\\Desktop\\selenium4-training\\chromedriver.exe");
 //           WebDriver driver = new ChromeDriver(); // Parent p=new Child (Webdriver is Parent & ChromeDriver is Child)
-//           driver.get("https://www.scrum.org/"); // opening url
+//           driver.get("https://demo.nopcommerce.com/"); // opening url
 //           System.out.println(driver.getTitle());
 //           driver.quit(); //closing url
        
@@ -23,7 +23,7 @@ public class browser {
 //    	//Firefox browser
 //    	System.setProperty("webdriver.gecko.driver","C:\\Users\\ghazi\\Desktop\\selenium4-training\\geckodriver.exe");
 //        WebDriver driver = new FirefoxDriver(); // Parent p=new Child (Webdriver is Parent & ChromeDriver is Child)
-//        driver.get("https://www.scrum.org/"); // opening url
+//        driver.get("https://demo.nopcommerce.com/"); // opening url
 //        System.out.println(driver.getTitle());
 //        driver.quit(); //closing url
         	
@@ -37,23 +37,23 @@ public class browser {
           WebDriverManager.chromedriver().setup();
           WebDriver driver = new ChromeDriver(); // Parent p=new Child (Webdriver is Parent & ChromeDriver is Child)       
         	
-            //Mazimize current window
-            driver.manage().window().maximize();
+            //Maximize current window
+            driver.manage().window().maximize();  
             
-          driver.get("https://www.scrum.org/"); // opening url
+          driver.get("https://demo.nopcommerce.com/"); // opening url
           System.out.println("the title of web page is:" +driver.getTitle());
           System.out.println("the url of web page is:" +driver.getCurrentUrl());
           driver.quit(); //closing url
         	
         	
 //        	//Firefox browser
-//        	WebDriverManager.firefoxdriver().setup();
+//        	  WebDriverManager.firefoxdriver().setup();
 //            WebDriver driver = new FirefoxDriver(); // Parent p=new Child (Webdriver is Parent & ChromeDriver is Child)
 //            
-//            //Mazimize current window
+//            //Maximize current window
 //            driver.manage().window().maximize();
 //            
-//            driver.get("https://www.scrum.org/"); // opening url                     
+//            driver.get("https://demo.nopcommerce.com/"); // opening url                     
 //            System.out.println("the title of web page is:" +driver.getTitle());
 //            System.out.println("the url of web page is:" +driver.getCurrentUrl());
 //            driver.quit(); //closing url
